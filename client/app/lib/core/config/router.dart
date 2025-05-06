85a425e310f45bb4536718d93a499d1387ac2bf7
aaaz425 <aaaz425@naver.com>
2025-05-07 00:40:54 +0900

    init: 라우터 1차 설정
    jira ref: #S12P31S303-20

--- client/app/lib/core/config/router.dart
+++ client/app/lib/core/config/router.dart
@@ -1,7 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../../../features/user/presentation/screens/user_screen.dart';
+import '../../../features/sleep_mode/presentation/screens/home_screen.dart';
+import '../../shared/widgets/shell_screen.dart';
 
 // Todo: 라우팅 설정
 final appRouter = GoRouter(
-  routes: [GoRoute(path: '/', builder: (context, state) => const UserScreen())],
+  routes: [
+    GoRoute(
+      path: '/',
+      pageBuilder:
+          (BuildContext context, GoRouterState state) =>
+              const NoTransitionPage(child: ShellScreen(body: HomeScreen())),
+    ),
+    GoRoute(
+      path: '/analysis',
+      pageBuilder:
+          (BuildContext context, GoRouterState state) =>
+              const NoTransitionPage(child: ShellScreen(body: HomeScreen())),
+    ),
+    GoRoute(
+      path: '/coach',
+      pageBuilder:
+          (BuildContext context, GoRouterState state) =>
+              const NoTransitionPage(child: ShellScreen(body: HomeScreen())),
+    ),
+    GoRoute(
+      path: '/sound',
+      pageBuilder:
+          (BuildContext context, GoRouterState state) =>
+              const NoTransitionPage(child: ShellScreen(body: HomeScreen())),
+    ),
+    GoRoute(
+      path: '/mypage',
+      pageBuilder:
+          (BuildContext context, GoRouterState state) =>
+              const NoTransitionPage(child: ShellScreen(body: HomeScreen())),
+    ),
+  ],
 );
