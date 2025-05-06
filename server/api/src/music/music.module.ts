05359aa0ef53b95011a41d0a85c7076118947d56
홍승찬 <hsch19@naver.com>
2025-05-06 18:49:52 +0900

    feat: 음악 정보 조회 API 추가(진행중)
    - 음악 ID로 특정 음악 정보 조회하는 API 추가
    
    jira ref: #S12P31S303-68

---
+++ server/api/src/music/music.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { MusicController } from "./music.controller";
+import { Music } from "./music.entity";
+import { MusicService } from "./music.service";
+
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([Music])
+    ],
+    controllers: [
+        MusicController
+    ],
+    providers: [
+        MusicService
+    ],
+    exports: [
+
+    ],
+})
+
+export class MusicModule {}
