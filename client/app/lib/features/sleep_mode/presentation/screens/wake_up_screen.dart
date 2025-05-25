4c76b12d87e879985ef279ae805d987fa17495b4
inaemin <inaemin@gmail.com>
2025-05-26 02:55:02 +0900

    design: 수면종료 화면 디자인 수정

--- client/app/lib/features/sleep_mode/presentation/screens/wake_up_screen.dart
+++ client/app/lib/features/sleep_mode/presentation/screens/wake_up_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sleep_tight/core/config/app_config.dart';
 import 'package:sleep_tight/core/config/theme/color.dart';
+import 'package:sleep_tight/core/config/theme/text_styles.dart';
 import 'package:sleep_tight/features/sleep_mode/presentation/widgets/progress_bar.dart';
+import 'package:sleep_tight/shared/widgets/custom_button.dart';
 
 class WakeUpScreen extends StatelessWidget {
   const WakeUpScreen({super.key});
@@ -23,82 +25,63 @@ class WakeUpScreen extends StatelessWidget {
                 child: ProgressBar(),
               ),
             ),
+            Expanded(
+              flex: 2,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/check_blue.png',
+                      width: 120,
+                      height: 120,
+                      fit: BoxFit.fitWidth,
+                    ),
+                    SizedBox(height: 12),
 
-            const SizedBox(height: 207),
-
-            Center(
-              child: Image.asset(
-                'assets/images/check_blue.png',
-                width: 120,
-                fit: BoxFit.fitWidth,
+                    Text(
+                      '편안한 밤 되셨나요?',
+                      style: AppTextStyles.titleT2Rg(color: AppColors.white),
+                    ),
+                  ],
+                ),
               ),
             ),
 
-            SizedBox(height: 12),
-
-            Text(
-              '편안한 밤 되셨나요?',
-              style: const TextStyle(
-                fontSize: 20,
-                color: AppColors.white,
-                fontWeight: FontWeight.w600,
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  CustomButton(
+                    width: 156,
+                    height: 48,
+                    onPressed: () {
+                      context.go(
+                        Uri(
+                          path: AppConfig.routes.sleepAnalysis,
+                          queryParameters: {'tab': 'diary'},
+                        ).toString(),
+                      );
+                    },
+                    text: '일지 작성하기',
+                    textStyle: AppTextStyles.button1Sb(color: AppColors.white),
+                    textColor: AppColors.white,
+                  ),
+                  SizedBox(height: 4),
+                  CustomButton(
+                    width: 156,
+                    height: 48,
+                    onPressed: () {
+                      context.go(AppConfig.routes.home);
+                    },
+                    text: '닫기',
+                    theme: 'text',
+                    textStyle: AppTextStyles.button1Sb(color: AppColors.font2),
+                    textColor: AppColors.font2,
+                  ),
+                ],
               ),
-            ),
-
-            SizedBox(height: 167),
-
-            Column(
-              children: [
-                ConstrainedBox(
-                  constraints: const BoxConstraints(maxWidth: 156),
-                  child: SizedBox(
-                    height: 48,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: AppColors.primary,
-                        foregroundColor: AppColors.white,
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6),
-                        ),
-                      ),
-                      onPressed: () {
-                        context.go(
-                          Uri(
-                            path: AppConfig.routes.sleepAnalysis,
-                            queryParameters: {'tab': 'diary'},
-                          ).toString(),
-                        );
-                      },
-                      child: const Text(
-                        '일지 작성하기',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ),
-                  ),
-                ),
-                ConstrainedBox(
-                  constraints: const BoxConstraints(maxWidth: 156),
-                  child: SizedBox(
-                    height: 48,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: AppColors.font2,
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6),
-                        ),
-                      ),
-                      onPressed: () {
-                        context.go(AppConfig.routes.home);
-                      },
-                      child: const Text('닫기', style: TextStyle(fontSize: 16)),
-                    ),
-                  ),
-                ),
-              ],
             ),
           ],
         ),
